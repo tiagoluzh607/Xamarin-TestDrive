d5453782dbe615ac8c9d9d435bba5d9436d44a74
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-09 10:10:33 -0200

    Login - Mensagem ao usuario quando esta sem conexão com a internet

--- AluraTestDrive/AluraTestDrive/LoginService.cs
+++ AluraTestDrive/AluraTestDrive/LoginService.cs
@@ -14,27 +14,32 @@ namespace AluraTestDrive
         public async Task LogarAsync(Login login)
         {
 
-            using (var cliente = new HttpClient())
+            try
             {
-                var camposFormulario = new FormUrlEncodedContent(new[]
+                using (var cliente = new HttpClient())
                 {
+                    var camposFormulario = new FormUrlEncodedContent(new[]
+                    {
                     new KeyValuePair<string, string>("email", login.email),
                     new KeyValuePair<string, string>("senha", login.senha)
                 });
 
-                cliente.BaseAddress = new Uri("https://aluracar.herokuapp.com");
-                var resposta = await cliente.PostAsync("/login", camposFormulario);
+                    cliente.BaseAddress = new Uri("https://aluracar.herokuapp.com");
+                    var resposta = await cliente.PostAsync("/login", camposFormulario);
 
-                if (resposta.IsSuccessStatusCode)
-                {
-                    MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin"); // quem implenta é App.xaml.cs justo para fazer a troca da mainpage e navegação
+                    if (resposta.IsSuccessStatusCode)
+                    {
+                        MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin"); // quem implenta é App.xaml.cs justo para fazer a troca da mainpage e navegação
+                    }
+                    else
+                    {
+                        MessagingCenter.Send<LoginException>(new LoginException("Usuário ou senha Incorretos"), "FalhaLogin");
+                    }
                 }
-                else
-                {
-                    MessagingCenter.Send<LoginException>(new LoginException("Usuário ou senha Incorretos"), "FalhaLogin");
-                }
-
-
+            }
+            catch (Exception)
+            {
+                MessagingCenter.Send<LoginException>(new LoginException("Ocorreu um erro de Comunicação com o Servidor.\n\n Por favor verifique sua conexão e tente mais tarde "), "FalhaLogin");
             }
         }
     }
