6166f5269040d3c69fabc29043cd4e18238b712c
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-14 13:21:29 -0200

    SQLite - Incluindo chave primaria

--- AluraTestDrive/AluraTestDrive/Models/AgendamentoBD.cs
+++ AluraTestDrive/AluraTestDrive/Models/AgendamentoBD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,7 +7,8 @@ namespace AluraTestDrive.Models
 {
     public class AgendamentoBD
     {
-        
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Fone { get; set; }
         public string Email { get; set; }
