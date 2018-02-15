75436982f1eec2dc556ab0bcaaf11fe18835153e
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-15 15:30:11 -0200

    Consulta Agendamentos - Trocando as cores quando um elemento nao esta sincronizado com o servidor / Usando Converters

---
+++ AluraTestDrive/AluraTestDrive/Converters/AgendamentoConfirmadoConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AluraTestDrive.Converters
+{
+    class AgendamentoConfirmadoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool confirmado = (bool)value;
+
+            if (confirmado)
+            {
+                return Color.DarkGreen;
+            }
+            else
+            {
+                return Color.DarkRed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
